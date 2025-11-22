c4152d01de5ee4afe6cfd3d4dacf413a27105a7a
Parker Bryan <parker@Parkers-MacBook-Pro.local>
2025-11-21 19:08:17 -0700

    v4.7 trying to make the keyboard not cover shit

--- src/App.tsx
+++ src/App.tsx
@@ -209,7 +209,7 @@ function App() {
           onTryDifferentPath={handleTryDifferentPath}
         />
       )}
-      <div className="app-version">v4.6</div>
+      <div className="app-version">v4.7</div>
     </div>
   );
 }
